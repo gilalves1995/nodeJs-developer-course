b2c02eaaea84430333cb3803a7ea2065eba1cbbf
Gil Alves <gg.alves@campus.fct.unl.pt>
2019-10-11 14:34:52 +0100

    Lesson 12: Printing in colour with chalk.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,18 +1,32 @@
-const validator = require('validator');
+// const validator = require('validator');
+
+const chalk = require('chalk');
 const getNotes = require('./notes');
 
 const notes = getNotes();
 console.log(notes);
 
-console.log(validator.isURL('https:/mead.io'));
+// Chalk - easily define themes
+const error = chalk.bold.red;
+const warning = chalk.keyword('orange');
 
 
+console.log(error("Error. Try again later."));
+console.log(warning("Dangerous. Are you sure you want to open the door?"));
+console.log(chalk.green("Success!"));
+
+console.log(chalk.bgRed.bold.inverse("Messing things up!"));
+
+
+
+
+
+// console.log(validator.isURL('https:/mead.io'));
 
 /*
 *  Use npm module system: first step is to initialize npm from the project's root
+*  Npm install generates the folder node_modules with package.json info
 * */
-
-
 
 
 
