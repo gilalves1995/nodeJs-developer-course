389005c43cc8b17a9853545535d5754a96ee87fc
Gil Alves <gg.alves@campus.fct.unl.pt>
2019-10-11 15:39:50 +0100

    Lesson 17: Argument parsing with yargs, part 2.

--- notes-app/app.js
+++ notes-app/app.js
@@ -12,8 +12,21 @@ const getNotes = require('./notes');
 yargs.command({
     command: 'add',
     describe: 'Adds a new note.',
-    handler: function() {
-        console.log('Adding a new note.');
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        console.log("Title: " + argv.title);
+        console.log("Body: " + argv.body);
     }
 });
 
@@ -44,7 +57,7 @@ yargs.command({
     }
 });
 
-console.log(yargs.argv);
+yargs.parse();
 
 
 
