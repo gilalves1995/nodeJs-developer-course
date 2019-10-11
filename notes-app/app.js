74487b3d71b6870240361011da20b0612eb087ef
Gil Alves <gg.alves@campus.fct.unl.pt>
2019-10-11 14:12:35 +0100

    Lesson 11: Npm module system overview.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,15 +1,23 @@
+const validator = require('validator');
 const getNotes = require('./notes');
 
 const notes = getNotes();
 console.log(notes);
 
+console.log(validator.isURL('https:/mead.io'));
+
+
+
+/*
+*  Use npm module system: first step is to initialize npm from the project's root
+* */
 
 
 
 
 
-
-// Provide relative path to utils.js
 // const add = require('./utils');
 // const sum = add(4, -2);
 // console.log(sum);
+
+
