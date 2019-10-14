89a3422ba0f28978117c8ec74d5eaaf18d5caa22
Gil Alves <gg.alves@campus.fct.unl.pt>
2019-10-14 18:27:12 +0100

    Lesson 38: Callback chainning and argument passing in Weather App.

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,12 +4,22 @@
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-geocode('Boston', (error, data) => {
-    console.log(error);
-    console.log(data);
+const location = process.argv[2];
+if (!location) {
+    return console.log("You must provide a location.");
+}
+
+geocode(location, (error, data) => {
+    if (error) {
+        return console.log(error);
+    }
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log(data.location);
+        console.log(forecastData);
+    });
 });
 
-forecast(-75.7088, 44.1545, (error, data) => {
-    console.log('Error', error);
-    console.log('Data', data);
-});
+
