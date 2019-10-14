08298012f9f58d68acf974a6132a909f1ae97710
Gil Alves <gg.alves@campus.fct.unl.pt>
2019-10-14 13:51:44 +0100

    Lesson 12: code refactoring to use arrow functions.

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -4,15 +4,13 @@ const chalk = require('chalk');
 const success = chalk.green.inverse;
 const error = chalk.red.bold.inverse;
 
-const getNotes = function() {
+const getNotes = () => {
     return "My notes...";
 };
 
-const addNote = function(title, body) {
+const addNote = (title, body) => {
     const notes = loadNotes();
-    const duplicateNotes = notes.filter(function(note) {
-        return note.title === title;
-    });
+    const duplicateNotes = notes.filter(note => note.title === title);
 
     if (duplicateNotes.length === 0) {
         notes.push({ title: title, body: body });
@@ -23,11 +21,9 @@ const addNote = function(title, body) {
     }
 };
 
-const removeNote = function(title) {
+const removeNote = title => {
     const notes = loadNotes();
-    const notesToKeep = notes.filter(function(note) {
-        return note.title !== title;
-    });
+    const notesToKeep = notes.filter(note => note.title !== title);
 
     if (notes.length > notesToKeep.length) {
         saveNotes(notesToKeep);
@@ -37,12 +33,12 @@ const removeNote = function(title) {
     }
 };
 
-const saveNotes = function(notes) {
+const saveNotes = notes => {
     const dataJSON = JSON.stringify(notes);
     fs.writeFileSync('notes.json', dataJSON);
 };
 
-const loadNotes = function() {
+const loadNotes = () => {
     try {
         const dataBuffer = fs.readFileSync('notes.json');
         const dataJSON = dataBuffer.toString();
