e0335de1aa267025302cce19f71a44e8dc61c676
Gil Alves <gg.alves@campus.fct.unl.pt>
2019-10-14 17:56:29 +0100

    Lesson 36: Callback abstraction.

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,10 +1,11 @@
 // Single-threaded - only one thing can be executed at a time
 // Non-blocking - waiting for a task to finish does not causes the execution to block (callbacks)
 
+
 const request = require('request');
+const geocode = require('./utils/geocode');
 
 const url = 'https://api.darksky.net/forecast/9bf79a7dd4e3e589442f0f7f29ff283f/37.8267,-122.4233?units=si';
-const geocodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZ2ctYWx2ZXMiLCJhIjoiY2sxcWt1NWg3MTU4cDNjbzY4ZGhuNzlhaCJ9.ECEBGmfVNUzZedFGWvDuKQ&limit=1';
 
 /*
 request({ url: url, json: true }, (error, response) => {
@@ -19,14 +20,7 @@ request({ url: url, json: true }, (error, response) => {
 */
 
 
-request({ url: geocodeUrl, json: true }, (error, response) => {
-    if (error) {
-        console.log('Unable to connect to geocoding service.');
-    } else if (response.body.features.length === 0) {
-        console.log('Unable to find the location.');
-    } else {
-        const latitude = response.body.features[0].center[1];
-        const longitude = response.body.features[0].center[0];
-        console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
-    }
+geocode('Boston', (error, data) => {
+    console.log(error);
+    console.log(data);
 });
