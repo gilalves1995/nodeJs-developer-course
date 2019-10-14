784cfbd2f949e3dc1db075df3a08013dd1c775e1
Gil Alves <gg.alves@campus.fct.unl.pt>
2019-10-14 16:21:32 +0100

    Lesson 32: Customizing HTTP requests with DarkSky.

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,9 +3,8 @@
 
 const request = require('request');
 
-const url = 'https://api.darksky.net/forecast/9bf79a7dd4e3e589442f0f7f29ff283f/37.8267,-122.4233';
+const url = 'https://api.darksky.net/forecast/9bf79a7dd4e3e589442f0f7f29ff283f/37.8267,-122.4233?units=si';
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body);
-    console.log(data.currently);
+request({ url: url, json: true }, (error, response) => {
+    console.log(`${response.body.daily.data[0].summary} It is currently ${response.body.currently.temperature} degrees out. There is a ${response.body.currently.precipProbability}% chance of rain.`);
 });
