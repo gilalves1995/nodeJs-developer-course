60ee805b3283f66b16762ba55d3e48c5afd7eaba
Gil Alves <gg.alves@campus.fct.unl.pt>
2019-10-15 11:14:44 +0100

    Lesson 40: Weather app refactoring to use object shorthand and destructuring.

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,20 +4,20 @@
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-const location = process.argv[2];
-if (!location) {
+const address = process.argv[2];
+if (!address) {
     return console.log("You must provide a location.");
 }
 
-geocode(location, (error, data) => {
+geocode(address, (error, { latitude, longitude, location }) => {
     if (error) {
         return console.log(error);
     }
-    forecast(data.latitude, data.longitude, (error, forecastData) => {
+    forecast(latitude, longitude, (error, forecastData) => {
         if (error) {
             return console.log(error);
         }
-        console.log(data.location);
+        console.log(location);
         console.log(forecastData);
     });
 });
--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -2,13 +2,13 @@ const request = require('request');
 
 const forecast = (latitude, longitude, callback) => {
     const url = `https://api.darksky.net/forecast/9bf79a7dd4e3e589442f0f7f29ff283f/${latitude},${longitude}?units=si`;
-    request({ url: url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service.', undefined);
-        } else if(response.body.error) {
+        } else if(body.error) {
             callback('Unable to find provided location.', undefined);
         } else {
-            callback(undefined, `${response.body.daily.data[0].summary} It is currently ${response.body.currently.temperature} degrees out. There is a ${response.body.currently.precipProbability}% chance of rain.`);
+            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain.`);
         }
     });
 };
