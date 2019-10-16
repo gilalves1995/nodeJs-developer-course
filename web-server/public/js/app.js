d1a98ca6db97cbcccec566646040ff053dc628a9
Gil Alves <gg.alves@campus.fct.unl.pt>
2019-10-16 18:55:50 +0100

    Lesson 57: Using fetch to request data on client-side.

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,15 @@
+// This is file is loaded by index.hbs and contains the logic on client-side
+// Here we intend to issue a request to the server with fetch to get the weather data
+
 console.log('Client-side javascript file is loaded!');
+
+fetch('http://localhost:3000/weather?address=Boston').then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            return console.log(data.error);
+        }
+
+        console.log("Forecast: ", data.forecast);
+        console.log("Location: ", data.location);
+    });
+});
